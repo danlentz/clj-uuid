9c7556d840cf653c3daa5482085f0881eed6d968
Dan Lentz <danlentz@github.com>
2013-11-03 16:33:04 -0500

    need explicit jsr166y dep for core.reducers

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject danlentz/clj-uuid "0.0.4-SNAPSHOT"
+(defproject danlentz/clj-uuid "0.0.5-SNAPSHOT"
   :description  "A Clojure library for generation and utilization of
                 UUIDs (Universally Unique Identifiers) as described by
                 RFC-4122.  The essential nature of the service it provides
@@ -9,4 +9,5 @@
   :license      {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                  [primitive-math     "0.1.3"]])
+                 [org.codehaus.jsr166-mirror/jsr166y "1.7.0"]
+                 [primitive-math     "0.1.3"]])
