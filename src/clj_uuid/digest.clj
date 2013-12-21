769a4c1051f3936f67dd1f33dbd9b700c5cfa516
Dan Lentz <danlentz@github.com>
2013-12-21 09:27:21 -0500

    fix: digest-uuid-bytes result now congruent with ironclad:digest-sequence

--- src/clj_uuid/digest.clj
+++ src/clj_uuid/digest.clj
@@ -24,7 +24,7 @@
 
 (defmethod sha1 String [s]
   (sbvec
-    (map sb8
+    (seq
       (-> (make-digest +sha1+)
         (.digest (.getBytes s))))))
 
@@ -33,17 +33,14 @@
 (=    (sha1 "xyz") (sha1 "xyz"))
 (not= (sha1 "xyz") (sha1 "x"))
 
-
 (defmethod sha1 clojure.core.Vec [coll]
   (sbvec
-    (map sb8 
+    (seq
       (-> (make-digest +sha1+)
         (.digest (byte-array (map sb8 coll)))))))
 
-                   ;; (apply concat (map #(cond
-                   ;;                                 (coll? %) (.getBytes %)
-                   ;;                                 :else [(sb8 %)])
-                   ;;                           coll))))))))
+(defmethod sha1 clojure.lang.PersistentVector [coll]
+  (sha1 (sbvec coll)))
 
 (=    (sha1 (sha1 "x")) (sha1 (sha1 "x")))
 (=    (sha1 (sha1 "xyz")) (sha1 (sha1 "xyz")))
@@ -61,9 +58,9 @@
 
 (defmethod md5 String [s]
   (sbvec
-    (map sb8
+    (seq
       (-> (make-digest +md5+)
-        (.digest (.getBytes s))))))
+          (.digest (.getBytes s))))))
 
 (= (md5 "")    (md5 ""))
 (= (md5 "x")   (md5 "x"))
@@ -73,9 +70,13 @@
 
 (defmethod md5 clojure.core.Vec [coll]
   (sbvec
-    (map sb8 
+    (seq 
       (-> (make-digest +md5+)
-        (.digest (byte-array (map sb8 coll)))))))
+          (.digest (byte-array (map sb8 coll)))))))
+
+(defmethod md5 clojure.lang.PersistentVector [coll]
+  (md5 (sbvec coll)))
+
 
 (=    (md5 (md5 "x"))   (md5 (md5 "x")))
 (=    (md5 (md5 "xyz")) (md5 (md5 "xyz")))
@@ -90,24 +91,11 @@
 ;; Digest Namespaced Identifiers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (defn digest-uuid-str [digest uuid & namestring-parts]
-;;  (subvec (digest (vec (cons (.toString uuid) namestring-parts))) 0 16))
-
 (defn digest-uuid-bytes [digest uuid-bytes namestring]
   (apply sbvector
-    (subvec (digest
-              (concat uuid-bytes
-                (seq (.getBytes namestring))))
-      0 16)))
-
-(= (digest-uuid-bytes sha1 (concat (sbvec -1) (sbvec -1)) "")
-  (sbvec [-73 46 20 91 7 104 59 84 103 -52 -118 -53 -39 -36 91 -37]))
-(= (digest-uuid-bytes sha1 (concat (sbvec -1111) (sbvec -1111)) "x")
-  (sbvec [51 88 53 -106 45 19 -86 -76 -104 -125 -70 103 15 12 26 -101]))
-(= (digest-uuid-bytes md5 (concat (sbvec -1) (sbvec -1)) "")
-  (sbvec [-101 -52 -64 -103 -26 -111 116 54 -45 -42 121 26 -110 -52 112 28]))
-(= (digest-uuid-bytes md5 (concat (sbvec -1111) (sbvec -1111)) "x")
-  (sbvec [-31 -19 -78 58 -47 35 100 -64 43 -63 -44 127 -113 24 37 -3]))
+         (subvec (digest (sbvec (concat uuid-bytes
+                                        (seq (.getBytes namestring)))))
+                 0 16)))
 
 
 
