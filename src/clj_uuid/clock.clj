16d2308ce60b7baf622088c405eb6add4d8f5b21
Dan Lentz <danlentz@gmail.com>
2015-01-31 16:34:05 -0500

    updated: explicit destructuring more efficient than apply

--- src/clj_uuid/clock.clj
+++ src/clj_uuid/clock.clj
@@ -1,14 +1,16 @@
-(ns clj-uuid.clock)
+(ns clj-uuid.clock
+  (:import [clojure.lang MapEntry]))
+  
 
 (set! *warn-on-reflection* true)
 
 (def ^:const +tick-resolution+  9999)
 
-(defn- pair ^clojure.lang.MapEntry [k v]
-  (clojure.lang.MapEntry. ^short k ^long v))
+(defn- pair ^MapEntry [k v]
+  (MapEntry. ^short k ^long v))
 
 
-(def state              (atom ^clojure.lang.MapEntry (pair 0 0)))
+(def state              (atom ^MapEntry (pair 0 0)))
 (def clock-seq          (atom ^short (+ (rand-int 9999) 1)))
 
 
@@ -24,16 +26,17 @@
 (defn monotonic-time []
   (letfn [(timestamp []
             (+ (* (System/currentTimeMillis) 10000)  100103040000000000))]
-    (apply +
-      (swap! state
-        (fn ^clojure.lang.MapEntry [^clojure.lang.MapEntry current-state]
-          (loop [^long time-now (timestamp)]
-            (if-not (= (.val current-state) time-now)
-              (pair 0 time-now)
-              (let [tt (.key current-state)]
-                (if (< tt +tick-resolution+)
-                  (pair (inc tt) time-now)
-                  (recur (timestamp)))))))))))
+    (let [^MapEntry new-state
+          (swap! state
+            (fn ^MapEntry [^MapEntry current-state]
+              (loop [^long time-now (timestamp)]
+                (if-not (= (.val current-state) time-now)
+                  (pair 0 time-now)
+                  (let [tt (.key current-state)]
+                    (if (< tt +tick-resolution+)
+                      (pair (inc tt) time-now)
+                      (recur (timestamp))))))))]
+      (+ (.key new-state) (.val new-state)))))
 
 
 (set! *warn-on-reflection* false)
