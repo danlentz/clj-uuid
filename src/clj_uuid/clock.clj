2fed64a6ed88c21552cfe06622fd6a440198b558
Dan Lentz <danlentz@gmail.com>
2015-02-02 22:39:36 -0500

    updated: monotonic-time - a few more refinements

--- src/clj_uuid/clock.clj
+++ src/clj_uuid/clock.clj
@@ -1,45 +1,42 @@
 (ns clj-uuid.clock)
   
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Thread-safe Monotonically Increasing Timestamp Generator
+;; Lock-Free Thread-safe Monotonic Clock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; we pad to achieve the rfc4122 compat and support atomic incremental
+;; uuids-this-second subcounter on the low order bits, guarantee that
+;; never collides regardless of clock precision:
+;;
+;; 113914335216380000    (+ (* (epoch-time) 10000) 100103040000000000)
+;; 113914335216380001    first contending timestamp
+;; 113914335216380002    second contending timestamp
+;; ...                   and so forth
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (set! *warn-on-reflection* true)
 
-(def ^:const +tick-resolution+  9999)
+(def ^:const +subcounter-resolution+  9999)
 (def ^:const +clock-seq+        (+ (rand-int 9999) 1))
 
 
-(deftype Pair [^short k ^long v])
+(deftype State [^short seqid ^long millis])
 
-(defn- pair ^Pair [k v]
-  (Pair. k v))
-
-(def state              (atom (pair 0 0)))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; we pad to achieve the rfc4122 compat and support atomic incremental
-;; uuids-this-second subcounter on the low order bits, guarantee that never
-;; collides regardless of clock precision:
-;;
-;; 113914335216380000  = (+ (* (get-epoch-time)   10000)    100103040000000000)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defn monotonic-time ^long []
-  (let [^Pair new-state
-        (swap! state
-          (fn [^Pair current-state]
-            (loop [time-now (System/currentTimeMillis)]
-              (if-not (= (.v current-state) time-now)
-                (pair 0 time-now)
-                (let [tt (.k current-state)]
-                  (if (< tt +tick-resolution+)
-                    (pair (inc tt) time-now)
-                    (recur (System/currentTimeMillis))))))))]
-    (+ (.k new-state) (+ (* (.v new-state) 10000) 100103040000000000))))
+(let [-state- (atom (->State 0 0))]
+  (defn monotonic-time ^long []
+    (let [^State new-state
+          (swap! -state-
+            (fn [^State current-state]
+              (loop [time-now (System/currentTimeMillis)]
+                (if-not (= (.millis current-state) time-now)
+                  (->State 0 time-now)
+                  (let [tt (.seqid current-state)]
+                    (if (< tt +subcounter-resolution+)
+                      (->State (inc tt) time-now)
+                      (recur (System/currentTimeMillis))))))))]
+      (+ (.seqid new-state)
+        (+ (* (.millis new-state) 10000) 100103040000000000)))))
 
 
 (set! *warn-on-reflection* false)
