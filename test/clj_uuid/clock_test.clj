4c0ee7171a270a2f5a7be838f391bda015ded443
Dan Lentz <danlentz@gmail.com>
2015-02-04 01:19:13 -0500

    updated: placeholding

--- test/clj_uuid/clock_test.clj
+++ test/clj_uuid/clock_test.clj
@@ -3,16 +3,3 @@
     [clojure.test   :refer :all]
     [clj-uuid.clock :refer :all]))
 
-
-(deftest check-node-id
-  (is (= (make-node-id) (make-node-id) +node-id+)))
-
-(deftest check-nanosec-monotonic
-  (is (apply <= (repeat 100000 (get-internal-real-time)))))
-
-(deftest check-millisec-monotonic
-  (is (apply <= (repeat 10000   (get-universal-time)))))
-
-(deftest check-cooked-timestamp-monotonic
-  (is (apply <= (repeat 100000 (make-timestamp)))))
-
---
+++ test/clj_uuid/node_test.clj
@@ -0,0 +1,5 @@
+(ns clj-uuid.node-test
+  (:require
+    [clojure.test   :refer :all]
+    [clj-uuid.node :refer :all]))
+
