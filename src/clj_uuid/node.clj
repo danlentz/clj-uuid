1cf80361d2cca4514287ddd44c982613d5e56473
Dan Lentz <danlentz@gmail.com>
2015-02-04 02:02:34 -0500

    new: invoke jdk6 compatible node-id if necessary

--- src/clj_uuid/node.clj
+++ src/clj_uuid/node.clj
@@ -1,13 +1,9 @@
 (ns clj-uuid.node
   (:require [clj-uuid.bitmop :only [ubvec ub8]])
-  (:import  [java.net         InetAddress
-                              NetworkInterface]
-            [java.sql         Timestamp]
+  (:import  [java.net         InetAddress NetworkInterface]
             [java.security    MessageDigest]
-            [java.util        Properties
-                             Date]
+            [java.util        Properties]
             [java.nio.charset StandardCharsets]))
-
 
 
 (defn all-local-addresses []
@@ -25,29 +21,42 @@
       (map str (InetAddress/getAllByName host-name)))))
 
 
-(def node-id
-  (memoize
-   (fn []
-     (let [addresses (all-local-addresses)
-           ^MessageDigest digest (MessageDigest/getInstance "MD5")
-           ^Properties props (System/getProperties)
-           to-digest (reduce (fn [acc key]
-                               (conj acc (.getProperty props key)))
-                             addresses
-                             ["java.vendor"
-                              "java.vendor.url"
-                              "java.version"
-                              "os.arch"
-                              "os.name"
-                              "os.version"])]
-       (doseq [^String d to-digest]
-         (.update digest
-           (.getBytes d StandardCharsets/UTF_8)))
-       (->> (.digest digest)
-         (map clj-uuid.bitmop/ub8)
-         clj-uuid.bitmop/ubvec
-         (#(subvec % 0 6)))))))
+(defn- make-node-id-jdk6 []
+  (clj-uuid.bitmop/ubvec
+    (map clj-uuid.bitmop/ub8
+      (.getHardwareAddress
+        (java.net.NetworkInterface/getByInetAddress 
+          (java.net.InetAddress/getLocalHost))))))
 
 
-(def +node-id+          (node-id))
+(defn- make-node-id-jdk7 []
+  (let [addresses (all-local-addresses)
+        ^MessageDigest digest (MessageDigest/getInstance "MD5")
+        ^Properties    props  (System/getProperties)
+        to-digest (reduce (fn [acc key]
+                            (conj acc (.getProperty props key)))
+                    addresses
+                    ["java.vendor"
+                     "java.vendor.url"
+                     "java.version"
+                     "os.arch"
+                     "os.name"
+                     "os.version"])]
+    (doseq [^String d to-digest]
+      (.update digest
+        (.getBytes d StandardCharsets/UTF_8)))
+    (->> (.digest digest)
+      (map clj-uuid.bitmop/ub8)
+      clj-uuid.bitmop/ubvec
+      (#(subvec % 0 6)))))
+
+
+(defn- make-node-id []
+  (if (neg? (compare (System/getProperty "java.version") "1.7"))
+    (make-node-id-jdk6)
+    (make-node-id-jdk7)))
+
+(def node-id   (memoize make-node-id))
+
+(def +node-id+ (node-id))
 
