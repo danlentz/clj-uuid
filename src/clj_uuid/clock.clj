3067a584816da1a143064102a7cb328f5944aa9c
Dan Lentz <danlentz@gmail.com>
2015-02-04 22:07:04 -0500

    updated: monotonic time respects standard uuid epoch

--- src/clj_uuid/clock.clj
+++ src/clj_uuid/clock.clj
@@ -4,14 +4,29 @@
 ;; Lock-Free Thread-safe Monotonic Clock
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Universal time is represented as the number of seconds that have
+;; elapsed since 00:00 January 1, 1900 GMT.
+
+;; POSIX time is represented as the number of s
+;; between the current time and midnight, January 1, 1970 UTC
+
+;; Java time is represented as the difference, measured in milliseconds,
+;; between the current time and midnight, January 1, 1970 UTC
+
+;; UUID Epoch is 00:00 October 15, 1582 UTC
+
+;; Difference between UUID Epoch and Java Epoch = 141427 days =
+;; 12219292800 seconds
+
 ;; we pad to achieve the rfc4122 compat and support atomic incremental
 ;; uuids-this-second subcounter on the low order bits, guarantee that
 ;; never collides regardless of clock precision:
 ;;
-;; 113914335216380000    (+ (* (epoch-time) 10000) 100103040000000000)
-;; 113914335216380001    first contending timestamp
-;; 113914335216380002    second contending timestamp
-;; ...                   and so forth
+;; 113914335216380000  (+ (* (universal-time) 10000) 100103040000000000)
+;; 113914335216380001  first contending timestamp
+;; 113914335216380002  second contending timestamp
+;; ...                 and so forth
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -20,8 +35,8 @@
 (def ^:const +subcounter-resolution+  9999)
 (def ^:const +clock-seq+        (+ (rand-int 9999) 1))
 
-
 (deftype State [^short seqid ^long millis])
+
 
 (let [-state- (atom (->State 0 0))]
   (defn monotonic-time ^long []
@@ -35,8 +50,14 @@
                     (if (< tt +subcounter-resolution+)
                       (->State (inc tt) time-now)
                       (recur (System/currentTimeMillis))))))))]
-      (+ (.seqid new-state)
-        (+ (* (.millis new-state) 10000) 100103040000000000)))))
+      (+ (.seqid new-state)       
+        (+ 100103040000000000
+          (* (+ 2208988800000 (.millis new-state)) 10000))))))
 
 
+(defn universal-time []
+  (+ (quot (System/currentTimeMillis) 1000) 2208988800))
+
+
+  
 (set! *warn-on-reflection* false)
