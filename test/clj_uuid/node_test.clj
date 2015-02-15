2f358c58953238fbe5e2dd709a839e05ad7298c4
Dan Lentz <danlentz@gmail.com>
2015-02-15 13:26:58 -0500

    new: node-id test

--- test/clj_uuid/node_test.clj
+++ test/clj_uuid/node_test.clj
@@ -1,5 +1,12 @@
 (ns clj-uuid.node-test
-  (:require
-    [clojure.test   :refer :all]
-    [clj-uuid.node :refer :all]))
+  (:require [clojure.test   :refer :all]
+            [clj-uuid.node  :refer :all]))
+
+
+(deftest check-node-id
+  (testing "existance and type of node id...")
+  (is (= (node-id) +node-id+))
+  (is (coll? (node-id)))
+  (is (= 6 (count +node-id+)))
+  (is (every? number?  +node-id+)))
 
