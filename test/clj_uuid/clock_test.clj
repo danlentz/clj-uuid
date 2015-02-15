f0079f6fe2ea6ae76a89f2d85b808c2435d26cd3
Dan Lentz <danlentz@gmail.com>
2015-02-15 18:06:26 -0500

    new: check-monotonic-time

--- test/clj_uuid/clock_test.clj
+++ test/clj_uuid/clock_test.clj
@@ -1,5 +1,47 @@
 (ns clj-uuid.clock-test
-  (:require
-    [clojure.test   :refer :all]
-    [clj-uuid.clock :refer :all]))
+  (:require [clojure.test   :refer :all]
+            [clojure.set]
+            [clj-uuid.clock :refer :all]))
+
+
+(deftest check-monotonic-time
+  (doseq [concur (range 4 9)]
+    (let [extent    250000
+          agents    (map agent (repeat concur nil))
+          working   (map #(send-off %
+                            (fn [state]
+                              (repeatedly extent monotonic-time)))
+                      agents)
+          _         (apply await working)
+          answers   (map deref working)]
+      (testing "single-thread timestamp uniqueness..."
+        (is (=
+              (* concur extent)
+              (apply + (map (comp count set) answers)))))
+      (testing "concurrent timestamp uniqueness..."
+        (is (=
+              (* concur extent)
+              (count (apply clojure.set/union (map set answers))))))
+      (testing "concurrent monotonic increasing..."
+        (is (every? identity
+              (map #(apply < %) answers)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
