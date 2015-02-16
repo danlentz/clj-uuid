02fab95be6229b86f64a98bdefb11bf72a51e68e
Dan Lentz <danlentz@gmail.com>
2015-02-16 13:17:46 -0500

    new: v1 concurrency tests

---
+++ test/clj_uuid/v1_test.clj
@@ -0,0 +1,47 @@
+(ns clj-uuid.v1-test
+  (:require [clojure.test   :refer :all]
+            [clojure.set]
+            [clj-uuid :refer :all]))
+
+
+(deftest check-v1
+  (doseq [concur (range 5 9)]
+    (let [extent    100000
+          agents    (map agent (repeat concur nil))
+          working   (map #(send-off %
+                            (fn [state]
+                              (repeatedly extent v1)))
+                      agents)
+          _         (apply await working)
+          answers   (map deref working)]
+      (testing "single-thread v1 uuid uniqueness..."
+        (is (=
+              (* concur extent)
+              (apply + (map (comp count set) answers)))))
+      (testing "concurrent v1 uuid uniqueness..."
+        (is (=
+              (* concur extent)
+              (count (apply clojure.set/union (map set answers))))))
+      (testing "concurrent v1 monotonic increasing..."
+        (is (every? identity
+              (map #(apply < (map get-timestamp %)) answers)))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
