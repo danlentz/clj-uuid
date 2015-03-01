0848c9bd2dcea1a709f09070f3cb698874687493
Dan Lentz <danlentz@gmail.com>
2015-03-01 11:18:58 -0500

    new: v4 special case tests

--- test/clj_uuid/v4_test.clj
+++ test/clj_uuid/v4_test.clj
@@ -1,9 +1,14 @@
 (ns clj-uuid.v4-test
   (:require [clojure.test   :refer :all]
-            [clj-uuid :refer :all]))
+            [clj-uuid       :refer :all]))
 
 
-(deftest check-v4-empiracally
-  (testing "v4..."
-    (is true)))
+(deftest check-v4-special-cases
+  (testing "v4 special case correctness..."
+    (is (= (v4  0  0)  #uuid "00000000-0000-4000-8000-000000000000"))
+    (is (= (v4  0  1)  #uuid "00000000-0000-4000-8000-000000000001"))
+    (is (= (v4  0 -1)  #uuid "00000000-0000-4000-bfff-ffffffffffff"))
+    (is (= (v4 -1  0)  #uuid "ffffffff-ffff-4fff-8000-000000000000"))
+    (is (= (v4 -1 -1)  #uuid "ffffffff-ffff-4fff-bfff-ffffffffffff"))))
+
 
