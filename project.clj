251088fb84426c2c02672c683b628170552fb047
Dan Lentz <danlentz@gmail.com>
2015-03-14 17:08:27 -0400

    updated: working version (non-snapshot)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject danlentz/clj-uuid "0.1.3-SNAPSHOT"
+(defproject danlentz/clj-uuid "0.1.4"
   :description  "A Clojure library for generation and utilization of
                 UUIDs (Universally Unique Identifiers) as described by
                 RFC-4122. This library extends the standard Java UUID class
