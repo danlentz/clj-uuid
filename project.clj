62a78d0c19bcab3bca9bac6815a784406d1f0795
Dan Lentz <danlentz@gmail.com>
2019-06-07 17:09:48 -0400

    update signing key

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                 and manipulation of these UUIDs in a simple, efficient manner."
   :author       "Dan Lentz"
   :jvm-opts ^:replace []
-  :signing  {:gpg-key "3A2F2AA9"}
+  :signing  {:gpg-key "7FD20410"}
   :url          "https://github.com/danlentz/clj-uuid"
   :license      {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
