4b130292a23f840d9c1040ea98577d9c13db0a0a
Dan Lentz <dan.lentz@dean.com>
2024-09-24 12:42:05 -0400

    separate v4, v8 testing

--- test/clj_uuid/v4_test.clj
+++ test/clj_uuid/v4_test.clj
@@ -4,7 +4,6 @@
   (:require [clojure.test   :refer :all]
             [clj-uuid       :refer :all :exclude [> < =]]))
 
-
 (deftest check-v4-special-cases
   (testing "v4 special case correctness..."
     (is (= (v4  0  0)  #uuid "00000000-0000-4000-8000-000000000000"))
@@ -12,11 +11,3 @@
     (is (= (v4  0 -1)  #uuid "00000000-0000-4000-bfff-ffffffffffff"))
     (is (= (v4 -1  0)  #uuid "ffffffff-ffff-4fff-8000-000000000000"))
     (is (= (v4 -1 -1)  #uuid "ffffffff-ffff-4fff-bfff-ffffffffffff"))))
-
-(deftest check-v8-special-cases
-  (testing "v8 custom UUID"
-    (is (= (v8  0  0)  #uuid "00000000-0000-8000-8000-000000000000"))
-    (is (= (v8  0  1)  #uuid "00000000-0000-8000-8000-000000000001"))
-    (is (= (v8  0 -1)  #uuid "00000000-0000-8000-bfff-ffffffffffff"))
-    (is (= (v8 -1  0)  #uuid "ffffffff-ffff-8fff-8000-000000000000"))
-    (is (= (v8 -1 -1)  #uuid "ffffffff-ffff-8fff-bfff-ffffffffffff"))))
